7cf5fa028129470214233b156d127edc982d44fb
Kar-Tong Tan <ktan@g.harvard.edu>
2019-11-19 10:17:37 -0500

    cleaned up linear to perform addition with integer etc.

--- code/linear.py
+++ code/linear.py
@@ -1,3 +1,4 @@
+from dummy import dummy
 
 class AutoDiffToy():
 	'''
@@ -50,22 +51,36 @@ class AutoDiffToy():
 		or autodifftoyobject with a float/int
 		'''
 
-		# Assume that both objects are AutoDiffToyObjects
+		# Assumes that 'self' is an AutoDiff Object,
+		# and other is a number.
 		try:
-			alpha = self.alpha + other.alpha
-			beta = self.beta + other.beta
-			new_toy = AutoDiffToy(self.a, alpha, beta)
+			beta = self.beta + other.real
+			new_toy = AutoDiffToy(self.a, self.alpha, beta)
 			return(new_toy)
+		except AttributeError:
+			#raise AttributeError(f'{other.__class__.__name__}.{name} is invalid for multiplication.')
+			pass
 
-		# Perhaps the 'other' is not an AutoDiffToyObject.
-		# So we'll just add the constant values
+		# # Assume that both objects are AutoDiffToyObjects
+		# try:
+		# 	alpha = self.alpha + other.alpha
+		# 	beta = self.beta + other.beta
+		# 	new_toy = AutoDiffToy(self.a, alpha, beta)
+		# 	return(new_toy)
+		# # Perhaps the 'other' is not an AutoDiffToyObject.
+		# # So we'll just add the constant values
+		# except:
+
+		# We are adding two AutoDiff class, or an AutoDiff classs
+		# with another object here. In which case, we replace this
+		# with a dummy object and just retain the value and derivative
+		try:
+			self_dummy = dummy(self.val, self.der)
+			other_dummy = dummy(other.val, other.der)
+
+			return self_dummy + other_dummy
 		except:
-			try:
-				beta = self.beta + other.real
-				new_toy = AutoDiffToy(self.a, self.alpha, beta)
-				return(new_toy)
-			except:
-				raise AttributeError(f'{other.__class__.__name__}.{name} is invalid for multiplication.')
+			raise AttributeError()
 
 
 	def __radd__(self, other):
@@ -73,22 +88,86 @@ class AutoDiffToy():
 		Allows for commuative cases of addition, where a
 		float or integer are added to the autodifftoy object.
 		'''
-		# Assume that both objects are AutoDiffToyObjects
+		# We assume that 'other' is just a number being added 
+		# to the autodiff object. Allows commutative case
 		try:
-			alpha = self.alpha + other.alpha
-			beta = self.beta + other.beta
+			beta = self.beta + other.real
+			new_toy = AutoDiffToy(self.a, self.alpha, beta)
+			return(new_toy)
+		except AttributeError:
+			pass
+			#raise AttributeError(f'{other.__class__.__name__}.{name} is invalid for multiplication.')
+
+		# # Assume that both objects are AutoDiffToyObjects
+		# try:
+		# 	alpha = self.alpha + other.alpha
+		# 	beta = self.beta + other.beta
+		# 	new_toy = AutoDiffToy(self.a, alpha, beta)
+		# 	return(new_toy)
+		# # Perhaps the 'other' is not an AutoDiffToyObject.
+		# # So we'll just add the constant values
+		# except:
+
+		# We are adding two AutoDiff class, or an AutoDiff classs
+		# with another object here. In which case, we replace this
+		# with a dummy object and just retain the value and derivative
+		try:
+			self_dummy = dummy(self.val, self.der)
+			other_dummy = dummy(other.val, other.der)
+			return self_dummy + other_dummy
+		except:
+			raise AttributeError()
+
+
+	def __sub__(self, other):
+		# Assumes that 'self' is an AutoDiff Object,
+		# and other is a number.
+		try:
+			alpha = self.alpha
+			beta = self.beta - other.real
 			new_toy = AutoDiffToy(self.a, alpha, beta)
 			return(new_toy)
-		# Perhaps the 'other' is not an AutoDiffToyObject.
-		# So we'll just add the constant values
+		except AttributeError:
+			#raise AttributeError(f'{other.__class__.__name__}.{name} is invalid for multiplication.')
+			pass
+
+		# We are substracting two AutoDiff class, or another class with
+		# an AutoDiff class here. In which case, we replace this
+		# with a dummy object and just retain the value and derivative
+		try:
+			# Note that for a case of x - y, x refers to self here,
+			# and other refers to y.
+			self_dummy = dummy(self.val, self.der)
+			other_dummy = dummy(other.val, other.der)
+			return self_dummy - other_dummy
 		except:
-			try:
-				beta = self.beta + other.real
-				new_toy = AutoDiffToy(self.a, self.alpha, beta)
-				return(new_toy)
-			except:
-				#pass
-				raise AttributeError(f'{other.__class__.__name__}.{name} is invalid for multiplication.')
+			raise AttributeError()
+
+	def __rsub__(self, other):
+		# Assumes that 'self' is an AutoDiff Object,
+		# and other is a number.
+		# Allows commutative case.
+		# Note that for a case of x - y, y refers to self here,
+		# and other refers to x.
+		try:
+			alpha = self.alpha
+			beta = other.real - self.beta
+			new_toy = AutoDiffToy(self.a, alpha, beta)
+			return(new_toy)
+		except AttributeError:
+			#raise AttributeError(f'{other.__class__.__name__}.{name} is invalid for multiplication.')
+			pass
+
+		# We are substracting two AutoDiff class, or another class with
+		# an AutoDiff class here. In which case, we replace this
+		# with a dummy object and just retain the value and derivative
+		try:
+
+			self_dummy = dummy(self.val, self.der)
+			other_dummy = dummy(other.val, other.der)
+			return other_dummy - self_dummy
+		except:
+			raise AttributeError()
 
 
 	def __mul__(self, other):
@@ -96,15 +175,27 @@ class AutoDiffToy():
 		This allows for multiplication between a coefficient 
 		value and a AutoDiffToy object
 		'''
+		# We assume that 'other' is a number and try to 
+		# multiply this class ('self') with the number
 		# Multiply a number with a 'x' class
 		try:
 			alpha = self.alpha * other.real
-			new_toy = AutoDiffToy(self.a, alpha, self.beta)
+			beta = self.beta * other.real
+			new_toy = AutoDiffToy(self.a, alpha, beta)
 			return(new_toy)
+		except AttributeError:
+			#raise AttributeError(f'{other.__class__.__name__}.{name} is invalid for multiplication.')
+			pass
 
-		# Catch weird cases. E.g. when we're multiplying two 'x' classes
+		# We are multiplying two AutoDiff class, or an AutoDiff classs
+		# with another object here. In which case, we replace both of these
+		# with a dummy object and just retain the value and derivative
+		try:
+			self_dummy = dummy(self.val, self.der)
+			other_dummy = dummy(other.val, other.der)
+			return self_dummy * other_dummy
 		except:
-			raise AttributeError(f'{other.__class__.__name__}.{name} is invalid for multiplication.')
+			raise AttributeError()
 
 
 	def __rmul__(self, other):
@@ -115,11 +206,37 @@ class AutoDiffToy():
 		float class. (__mul__ function is called from the first variable
 		in a multiplication)
 		'''
+		# We assume that 'other' is a number and try to 
+		# multiply this class ('self') with the number
+		# Allows commutative case of multiply
 		try:
 			alpha = self.alpha * other.real
-			new_toy = AutoDiffToy(self.a, alpha, self.beta)
+			beta = self.beta * other.real
+			new_toy = AutoDiffToy(self.a, alpha, beta)
 			return(new_toy)
+		except AttributeError:
+			#raise AttributeError(f'{other.__class__.__name__}.{name} is invalid for multiplication.')
+			pass
+
+		# We are multiplying two AutoDiff class, or an AutoDiff classs
+		# with another object here. In which case, we replace both of these
+		# with a dummy object and just retain the value and derivative
+		try:
+			self_dummy = dummy(self.val, self.der)
+			other_dummy = dummy(other.val, other.der)
+			return other_dummy * self_dummy
 		except:
-			raise AttributeError(f'{other.__class__.__name__}.{name} is invalid for multiplication.')
+			raise AttributeError()
 
 
+	def __div__(self, other):
+		raise NotImplementedError
+
+	def __rdiv__(self, other):
+		raise NotImplementedError
+
+	def __pow__(self, other):
+		raise NotImplementedError
+
+	def __rpow__(self, other):
+		raise NotImplementedError
