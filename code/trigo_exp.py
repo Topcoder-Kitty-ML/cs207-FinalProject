981b790c55c545e88504fb3c446cdcf384931ec4
Kar-Tong Tan <ktan@g.harvard.edu>
2019-11-19 18:10:51 -0500

    fixed trigo_exp

--- code/trigo_exp.py
+++ code/trigo_exp.py
@@ -400,15 +400,11 @@ class trigo_exp():
 
 	def __rpow__(self, other): 
 		try:
-<<<<<<< HEAD
-			return self.__perform_division__(self, other)
-=======
-	
 			return self.__perform_higherorder__(self, other)
 		except:
 			raise AttributeError(f'{other.__class__.__name__}.{name} is invalid for higher order.')
 
-    @classmethod
+	@classmethod
 	def __perform_higherpower__(cls, self, other):
 		'''
 		Perform power
@@ -433,7 +429,6 @@ class trigo_exp():
 			self_dummy = dummy(self.val, self.der)
 			other_dummy = dummy(other.val, other.der)
 			return self_dummy ** other_dummy
->>>>>>> 236a78668d15e1c238579c78b51d556ddfd5c7c4
 		except:
 			raise AttributeError(f'{other.__class__.__name__}.{name} is invalid for higher order.')
 
