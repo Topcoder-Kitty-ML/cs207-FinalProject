5dc48977c6c9547452129437a8c86edec07952e9
Kar-Tong Tan <ktan@g.harvard.edu>
2019-12-05 14:36:26 -0500

    unprocessed data
    Former-commit-id: 9f8e28c8071c3719f6adcc901b1dec8c2ecfb98e

---
+++ adv_feature/rna_velocity/processed_data/extract_data.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+import numpy as np
+import loompy
+
+ds = loompy.connect("hgForebrainGlut.loom")
+
+spliced_layer = ds.layers["spliced"][:,:]
+unspliced_layer = ds.layers["unspliced"][:,:]
+
+spliced_layer.dump("hgForebrainGlut.spliced.pickle")
+unspliced_layer.dump("hgForebrainGlut.unspliced.pickle")
+
