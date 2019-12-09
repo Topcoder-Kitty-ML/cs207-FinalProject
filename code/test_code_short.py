cb812211a31c0458c93748f79f3e451aff195029
Tamilyn Chen <tamilynchen@dhcp-10-250-6-41.harvard.edu>
2019-12-09 13:51:28 -0500

    comparison tests to master

--- code/test_code_short.py
+++ code/test_code_short.py
@@ -10,7 +10,7 @@ print(x.val)
 print(x.der)
 
 z = x + y
-print(z.val)
+print(z.val) 
 print(z.der) 
 
 
@@ -29,6 +29,41 @@ z = x - y
 print(z.val)
 print(z.der) 
 
+x = Var(2)
+# y = Var(3)
+y = 3
+
+def test_GenerifDiff_lt:
+    f = GenericDiff(1, 0)
+    h = 2
+    assert (f.der < h.der)
+
+def test_GenerifDiff_gt:
+    f = GenericDiff(1, 0)
+    h = 2
+    assert (f.der > h.der)
+
+def test_GenerifDiff_eq():
+    f = GenericDiff(1, 0)
+    h = 2
+    assert (f.der == h.der)
+ 
+def test_GenerifDiff_ne:
+    f = GenericDiff(1, 0)
+    h = 2
+    assert (f.der != h.der)
+
+def test_GenerifDiff_le:
+    f = GenericDiff(1, 0)
+    h = 2
+    assert (f.der <= h.der)
+
+def test_GenerifDiff_ge:
+    f = GenericDiff(1, 0)
+    h = 2
+    assert (f.der >= h.der)
+    
+   
 z = y - x
 print(z.val)
 print(z.der) 
