b3925454370c685a24d3d2ce5bfd0e54979c7009
Kar-Tong Tan <ktan@g.harvard.edu>
2019-12-09 20:36:29 -0500

    optimization code

--- adv_feature/rna_velocity/functions.py
+++ adv_feature/rna_velocity/functions.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
 import math
+import sys
+sys.path.append('../../')
+from genericdiff import *
 
 def calc_du_dt(alpha, u_t):
 	'''
 	Calculate the rate of change of
 	the unspliced RNA (reactant) with
 	time.
-
 	Implements equation 3 in the RNA
 	velocity paper.
 	'''
@@ -21,7 +23,6 @@ def calc_ds_dt(gamma, u_t, s_t):
 	Calculate the rate of change of
 	the spliced RNA (product) with
 	time.
-
 	Implements equation 4 in the RNA
 	velocity paper.
 	'''
@@ -35,14 +36,13 @@ def calc_u(alpha, u_0, t):
 	Calculates the level of unspliced product (u)
 	at time t. Implements equation 5 in the 
 	RNA velocity paper.
-
 	Parameters:
 	alpha - Rate constant describing the transcription 
-	        rate
+			rate
 	u_0   - level of unspliced RNA (u) at time(t) = 0
 	t     - Time (t)
 	'''
-	result = alpha * (1 - math.exp(-1 * t)) + u_0 * math.exp(-1 * t)
+	result = alpha * (1 - exp(-1 * t)) + u_0 * exp(-1 * t)
 
 	return result
 
@@ -52,23 +52,22 @@ def calc_s(alpha, gamma, u_0, s_0, t):
 	Calculates the level of spliced product (s) at
 	time t. Implements equation 6 in the RNA velocity
 	paper.
-
 	Parameters:
 	alpha - Rate constant describing the transcription 
-	        rate
+			rate
 	gamma - Rate constant describing the degradation
-	        rate
+			rate
 	u_0   - level of unspliced RNA (u) at time(t) = 0
 	s_0   - level of spliced RNA (s) at time(t) = 0
 	t     - Time (t)
 	'''
-	result = (math.exp((-1*t) * (1+gamma)) * (math.exp(t*(1+gamma)) * \
-	            alpha * (gamma-1) + \
-	            math.exp(t*gamma)*(u_0-alpha)*gamma + \
-	            math.exp(t)*(alpha-gamma*(s_0+u_0+s_0*gamma)))) / \
-	            (gamma * (gamma-1))
+
+	result = (exp((-1*t) * (1+gamma)) * (exp(t*(1+gamma)) * \
+			alpha * (gamma-1) + \
+			exp(t*gamma)*(u_0-alpha)*gamma + \
+			exp(t)*(alpha-gamma*(s_0+u_0+s_0*gamma)))) / \
+			(gamma * (gamma-1))
+
 
 	
 	return result
-
-
